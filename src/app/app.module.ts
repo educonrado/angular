25e238fe7b6e14eb2935216c5a787acc3f6f42ea
edu <excz.test@gmail.com>
2019-01-18 19:46:55 +0000

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { RegisterPageComponent } from './componentes/register-page/register-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { PrivadoPageComponent } from './componentes/privado-page/privado-page.component';
+import { Page404Component } from './componentes/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    PrivadoPageComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
