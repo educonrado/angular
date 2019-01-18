1c945a7f0313f964e4b1a636f6a155c8e5fa589a
edu <excz.test@gmail.com>
2019-01-18 22:01:51 +0000

    Inicio con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBrG5zoJtqmnJH8DCTatlS2tvb0XufgN3Y",
+    authDomain: "project1-770a8.firebaseapp.com",
+    databaseURL: "https://project1-770a8.firebaseio.com",
+    projectId: "project1-770a8",
+    storageBucket: "project1-770a8.appspot.com",
+    messagingSenderId: "375166582085"
+  }
+};
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBrG5zoJtqmnJH8DCTatlS2tvb0XufgN3Y",
+    authDomain: "project1-770a8.firebaseapp.com",
+    databaseURL: "https://project1-770a8.firebaseio.com",
+    projectId: "project1-770a8",
+    storageBucket: "project1-770a8.appspot.com",
+    messagingSenderId: "375166582085"
+  }
 };
 
 /*
@@ -13,3 +21,4 @@ export const environment = {
  * because it will have a performance impact when errors are thrown
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
